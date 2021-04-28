c593efd88daaad60f1010d84128906459379a9ae
totallyNotAUser <68023215+totallyNotAUser@users.noreply.github.com>
2021-04-28 16:41:03 +0000

    Add some command line options

--- mkcvm.sh
+++ mkcvm.sh
@@ -30,14 +30,66 @@ echo '[***]   mkcvm by totallyNotAUser   [***]'
 echo '[***] Make a CVM mirror, and fast! [***]'
 echo
 
-echo '[*] Detecting package manager (distro)'
-if which apt > /dev/null 2>&1; then
-    packageman=apt
-elif which pacman > /dev/null 2>&1; then
-    packageman=pacman
+# default args
+default_tunnel_svc=lhr
+# parse args
+while [ "$#" -gt 0 ]
+do
+    case "$1" in
+        --help) echo "Usage: $0 <options>"
+                echo 'Options:'
+                echo '--help                    Print this help'
+                echo '-d, --distro <distro-id>  Force set distro'
+                echo '    Currently supported distros:'
+                echo '      | Distro Name | Distro-id |'
+                echo '      | Ubuntu      | ubu       |'
+                echo '      | Arch Linux  | arch      |'
+                echo '-t, --default-tunnel-service <service>'
+                echo '    Set a default tunneling service for the start script.'
+                echo '    Currently supported tunneling services:'
+                echo '      none - do not run a tunnel'
+                echo '      localhost.run (default) - shorthand lhr'
+                echo '      localtunnel (not tested yet) - shorthand lt'
+                echo '    Keep in mind: you can always override it using the --tunnel-service (-t) option of the start script.'
+                exit;;
+        -d|--distro)
+                distro_forced="yes"
+                case "$2" in
+                    ubu) distro="ubuntu"
+                    arch) distro="arch"
+                    *) echo "Unknown/unsupported distro $2"; exit 1;;
+                esac
+                shift 2;;
+        -t|--default-tunnel-service)
+                case "$2" in
+                    none) default_tunnel_svc="none";;
+                    localhost.run|lhr) default_tunnel_svc="lhr";;
+                    localtunnel|lt) default_tunnel_svc="lt";;
+                    *) echo "Unknown tunneling service $2"; exit 1;;
+                esac
+                shift 2;;
+        *) echo "Unknown option $1. Use --help for help."
+           exit 1;;
+    esac
+done
+
+if [ "$distro_forced" == yes ]
+then
+    echo "[*] Distro forced by user: $distro"
+    case "$distro" in
+        ubu) packageman=apt;;
+        arch) packageman=pacman;;
+    esac
 else
-    echo '[!] Unknown package manager (only apt and pacman are supported), failing'
-    exit 1
+    echo '[*] Detecting package manager (distro)'
+    if which apt > /dev/null 2>&1; then
+        packageman=apt
+    elif which pacman > /dev/null 2>&1; then
+        packageman=pacman
+    else
+        echo '[!] Unknown package manager (only apt and pacman are supported), failing'
+        exit 1
+    fi
 fi
 
 cd ~
@@ -77,9 +129,45 @@ cp notify* collab-vm
 
 echo [*] Making start script
 cat > ~/cvm-start.sh << EOF
+#!/bin/sh
+# defaults
+tunnel_svc=$default_tunnel_svc
+while [ "\$#" -gt 0 ]
+do
+    case "\$1" in
+        --help) echo "Usage: \$0 <options>"
+                echo 'Start script for cvm server, built by mkcvm'
+                echo 'Options:'
+                echo '--help    Print this help'
+                echo '-t, --tunnel-service <service>'
+                echo '    Choose a different tunneling service'
+                echo '    Currently supported tunneling services:'
+                echo '      none - do not run a tunnel'
+                echo '      localhost.run - shorthand lhr'
+                echo '      localtunnel (not tested yet) - shorthand lt'
+                echo "    Default set by mkcvm: $default_tunnel_svc"
+                exit;;
+        -t|--tunnel-service) 
+                case "\$2" in
+                    none) tunnel_svc="none";;
+                    localhost.run|lhr) tunnel_svc="lhr";;
+                    localtunnel|lt) tunnel_svc="lt";;
+                    *) echo "Unknown tunneling service \$2"
+                       exit 1;;
+                esac
+                shift 2;;
+        *) echo "Unknown option \$1. Use --help for help."
+           exit 1;;
+    esac
+done
 cd ~/cvm/final
 nohup ./collab-vm-server 8080 &
-ssh -R 80:localhost:8080 nokey@localhost.run
+# run tunnel
+case "\$tunnel_svc" in
+    none) echo 'Running no tunneling service, as requested.';;
+    lhr) ssh -R 80:localhost:8080 nokey@localhost.run;;
+    lt) npx localtunnel --port 8080;;
+esac
 EOF
 chmod +x ~/cvm-start.sh
 
